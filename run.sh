e9f43ae33b29272e613cc7ec2728b243ba3087be
speed1313 <speedtry13@icloud.com>
2023-08-14 13:17:07 +0900

    pass application

--- run.sh
+++ run.sh
@@ -9,9 +9,16 @@ CC=/opt/homebrew/opt/llvm/bin/clang
 
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
 
+OBJCOPY=/opt/homebrew/opt/llvm/bin/llvm-objcopy
+
+# build shell
+$CC $CFLAGS -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf shell.c user.c common.c
+$OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
+$OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
+
 # build kernel
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c common.c
+    kernel.c common.c shell.bin.o
 
 # run QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
---
+++ user.c
@@ -0,0 +1,20 @@
+#include "user.h"
+
+extern char __stack_top[];
+
+__attribute__((noreturn)) void exit(void) {
+    for (;;);
+}
+
+void putchar(char c) {
+    // TODO: implement this function
+}
+
+__attribute__((setion(".text.start")))
+__attribute__((naked))
+void start(void) {
+    __asm__ __volatile__(
+        "mv sp, %[stack_top]\n"
+        "call main\n"
+        "call exit\n" ::[stack_top] "r"(__stack_top));
+}
